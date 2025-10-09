fc8f1c3eb321bdef35cb38128ecce63df0b6fcb7
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-10-09 12:45:33 +0300

    Article.cs refactoring

--- backend/Data/Entities/Article.cs
+++ backend/Data/Entities/Article.cs
@@ -8,25 +8,37 @@ namespace backend.Data.Entities
 		public int ArticleId { get; set; }
 
 		public string? Slug { get; set; }
+
 		public string? Title { get; set; }
-		public string? Description { get; set; } // Subtitle
+
+		/// <summary>
+		/// Subtitle of the article.
+		/// </summary>
+		public string? Description { get; set; }
 
 		// TODO: A body is not just a string. There are complex formatting and links.
 		public string? Body { get; set; }
+
 		public Profile? Author { get; set; }
 
 		public List<Comment> Comments { get; set; } = new();
 
 		public List<string> Tags { get; set; } = new();
+
 		public bool Favorited { get; set; } // Dynamically generated
+
 		public int FovoritesCount { get; set; } // Dynamically generated
+
 		public string Image { get; set; } = string.Empty;
+
 		public int ClapsCount { get; set; } // Dynamically generated
+
 		public int CommentsCount { get; set; } // Dynamically generated
 
 		public Publication? Publication { get; set; }
 
 		public DateTime CreatedAt { get; set; }
+
 		public DateTime UpdatedAt { get; set; }
 
 		// TODO: Add tags, comments, favorites count, and the people who added this article as a favorite.
