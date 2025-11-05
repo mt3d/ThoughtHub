492d3a801f291a449dcba29e275bce31430d0ebf
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-11-05 08:37:37 +0300

    Add an initial ProfilesController to upload profile images

---
+++ backend/Controllers/ProfilesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using ThoughtHub.Data;
+using ThoughtHub.Services;
+
+namespace ThoughtHub.Controllers
+{
+	public class ProfilesController : Controller
+	{
+		private readonly PlatformContext _context;
+		private readonly IMediaService _mediaService;
+
+		public async Task<IActionResult> UploadProfileImage(IFormFile file)
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			var profileId = (await _context.Profiles.FirstAsync(p => p.UserId == userId)).ProfileId;
+
+			var url = _mediaService.UploadImageAsync(profileId, file);
+
+			return Ok(new { imageUrl = url });
+		}
+	}
+}
---
+++ backend/Services/IMediaService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThoughtHub.Services
+{
+	public interface IMediaService
+	{
+		string UploadImageAsync(int profileId, IFormFile file);
+	}
+}
