33f9fe2f56794714547efa662e4aa38d88b74493
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-11-18 19:49:34 +0300

    Add an empty Azure Blob Storage project

---
+++ ThoughtHub.Api.AzureBlobStorage/AzureBlobStorage.cs
@@ -0,0 +1,33 @@
+﻿using ThoughtHub.Data.Entities.Media;
+using ThoughtHub.Storage;
+
+namespace ThoughtHub.Api.AzureBlobStorage
+{
+	public class AzureBlobStorage : IStorage
+	{
+		public Task<bool> DeleteAsync(Image image)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> DeleteAsync(ImageVersion version)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> GetAsync(Image image, Stream stream)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string? GetPublicUrl(Image image)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string> PutAsync(Image image, Stream stream)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
