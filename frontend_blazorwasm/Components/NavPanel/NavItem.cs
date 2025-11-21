a338373306a07b0c1d4dcaf3f0ffa096d0affb31
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-11-21 09:39:20 +0300

    Add a NavItem class

---
+++ frontend_blazorwasm/Components/NavPanel/NavItem.cs
@@ -0,0 +1,31 @@
+﻿namespace ThoughtHub.UI.BlazorWasm.Components.NavPanel
+{
+	public class NavItem
+	{
+		/// <summary>
+		/// Custom css class
+		/// </summary>
+		public string? Class { get; set; }
+
+		public List<NavItem> ChildItems { get; set; }
+
+		public string? Description { get; set; }
+
+		public string? IconName { get; set; }
+
+		public bool IsEnabled { get; set; } = true;
+
+		public bool IsSeparator { get; set; }
+
+		public string? Id { get; set; }
+
+		/// <summary>
+		/// How to open the navigation item's link.
+		/// </summary>
+		public string? Target { get; set; }
+
+		public string Text { get; set; } = string.Empty;
+
+		public string? Url { get; set; }
+	}
+}
---
+++ frontend_blazorwasm/Components/NavPanel/Navigationpane.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace ThoughtHub.UI.BlazorWasm.Components.NavPanel
+{
+	public partial class NavigationPane<TItem> where TItem : class
+	{
+
+	}
+}
