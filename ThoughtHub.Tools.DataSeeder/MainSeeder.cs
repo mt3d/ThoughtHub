f7c6c84ff7f3de5b528756d09d87b2a19d66010c
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-11-24 22:11:16 +0300

    Make the header sticky

---
+++ ThoughtHub.Tools.DataSeeder/MainSeeder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThoughtHub.Tools.DataSeeder
+{
+	public class MainSeeder
+	{
+		public async Task SeedAllAsync(int articleCount = 1000)
+		{
+
+		}
+	}
+}
--- ThoughtHub.Tools.DataSeeder/Program.cs
+++ ThoughtHub.Tools.DataSeeder/Program.cs
@@ -2,11 +2,24 @@
 {
 	internal class Program
 	{
+		// TODO: Create a host
 		static void Main(string[] args)
 		{
 			try
 			{
+				var seedtype = args.Length > 0 ? args[0] : "all";
+				var count = args.Length > 1 && int.TryParse(args[1], out int c) ? c : 1000;
 
+				// var seeder = host.Services.GetRequiredService<MainSeeder>();
+				var seeder = new MainSeeder();
+
+				switch (seedtype.ToLower())
+				{
+					case "all":
+					default:
+						seeder.SeedAllAsync(count);
+						break;
+				}
 			}
 			catch (Exception ex)
 			{
