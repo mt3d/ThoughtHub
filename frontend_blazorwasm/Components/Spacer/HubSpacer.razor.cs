3a5890588a5b2be89dd293b04740752ca10392bd
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-12-05 12:37:53 +0300

    Add a spacer element

---
+++ frontend_blazorwasm/Components/Spacer/HubSpacer.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ThoughtHub.UI.BlazorWasm.Components.Spacer
+{
+	public partial class HubSpacer : HubComponentBase
+	{
+		[Parameter]
+		public int? Width { get; set; }
+
+		protected override string RootElementClass => "hub-spacer";
+
+		protected override void RegisterCssClasses()
+		{
+			StyleBuilder.Register(() => Width.HasValue ? $"margin-inline-start:{Width}px" : "flex-grow:1");
+		}
+	}
+}
