11b7d93c31fc3ef418fb8ff05d16c272cb2c6efa
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-12-05 22:06:01 +0300

    Add a basic tab component

---
+++ frontend_blazorwasm/Components/Tab/HubTab.razor.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ThoughtHub.UI.BlazorWasm.Components.Tab
+{
+	public partial class HubTab : HubComponentBase
+	{
+		private HubTabItem? _selectedItem;
+		private List<HubTabItem> _items = [];
+
+		[Parameter]
+		public RenderFragment? ChildContent { get; set; }
+
+		[Parameter]
+		public bool RenderAll { get; set; } = false;
+
+		[Parameter]
+		public HubTabPosition? Position { get; set; }
+
+		protected override string RootElementClass => "hub-tab";
+
+		public string? SelectedKey { get; set; }
+		public EventCallback<string?> SelectedKeyChanged { get; set; }
+
+		internal void RegisterItem(HubTabItem item)
+		{
+			if (SelectedKey is null)
+			{
+				if (_items.Count == 0)
+				{
+					item.SetIsSelected(true);
+					_selectedItem = item;
+					StateHasChanged();
+				}
+			}
+			else if (SelectedKey == item.Key)
+			{
+				item.SetIsSelected(true);
+				_selectedItem = item;
+				StateHasChanged();
+			}
+
+			_items.Add(item);
+		}
+
+		protected override void RegisterCssClasses()
+		{
+			ClassBuilder.Register(() => Position switch
+			{
+				HubTabPosition.Top => "hub-tab-top",
+				HubTabPosition.Bottom => "hub-tab-bottom",
+				HubTabPosition.Start => "hub-tab-start",
+				HubTabPosition.End => "hub-tab-end",
+				_ => "hub-tab-top"
+			});
+			base.RegisterCssClasses();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <remarks>
+		/// This method is called by the tab items when they the user clicks on them.
+		/// </remarks>
+		internal async void SelectItem(HubTabItem item)
+		{
+			_selectedItem?.SetIsSelected(false);
+			item.SetIsSelected(false);
+
+			_selectedItem = item;
+			await AssignSelectedKey(item.Key);
+
+			// TODO: Raise a tab change event.
+
+			StateHasChanged();
+		}
+
+		private async Task AssignSelectedKey(string? value)
+		{
+			SelectedKey = value;
+			await SelectedKeyChanged.InvokeAsync(value);
+		}
+	}
+}
---
+++ frontend_blazorwasm/Components/Tab/HubTabItem.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ThoughtHub.UI.BlazorWasm.Components.Tab
+{
+	public partial class HubTabItem : HubComponentBase
+	{
+		[CascadingParameter]
+		private HubTab? _parent { get; set; }
+
+		[Parameter]
+		public RenderFragment? Header { get; set; }
+
+		[Parameter]
+		public string? HeaderText { get; set; }
+
+		[Parameter]
+		public RenderFragment? Body { get; set; }
+
+		[Parameter]
+		public RenderFragment? ChildContent { get; set; }
+
+		[Parameter]
+		public string? Key { get; set; }
+
+		protected override string RootElementClass => "hub-tab-item";
+
+		protected override async Task OnInitializedAsync()
+		{
+			_parent?.RegisterItem(this);
+
+			await base.OnInitializedAsync();
+		}
+
+		// Two way bound.
+		public bool IsSelected { get; set; }
+
+		internal void SetIsSelected(bool value)
+		{
+			IsSelected = true;
+			StateHasChanged();
+		}
+
+		private async Task HandleOnClick()
+		{
+			// TODO: Check if the tab is enabled.
+
+			_parent?.SelectItem(this);
+		}
+	}
+}
