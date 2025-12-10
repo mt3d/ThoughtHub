5a84d87919a8e724e01a6629b847b675c8fe975c
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-12-10 21:35:27 +0300

    Add a BlockTypeAttribute to mark classes as blocks

---
+++ ThoughtHub.Api.Models/Content/BlockTypeAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace ThoughtHub.Api.Models.Content
+{
+	/// <summary>
+	/// Used to mark a class as a contnet block. Why use an attribute to store
+	/// these properties. Because these properties belongs to the whole class
+	/// (say HtmlBlock), not a single instance. So, adding them to the Block class itself
+	/// doesn't make much sense.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public class BlockTypeAttribute : Attribute
+	{
+		public string Name { get; set; }
+
+		public string Category { get; set; }
+
+		public string Icon { get; set; }
+
+		public string Component { get; set; }
+	}
+}
